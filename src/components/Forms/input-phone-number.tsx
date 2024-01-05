885765577d36597615cc6fd77c78f6d28dd08fef
iAfolayan <iafolayanibikunle@gmail.com>
2024-01-05 20:15:29 +0100

    fix phone number with code dropdown

--- src/components/Forms/input-phone-number.tsx
+++ src/components/Forms/input-phone-number.tsx
@@ -1,83 +1,46 @@
-import { clsx } from "clsx";
-interface IProps {
-  type: string;
-  label?: string;
-  placeholder?: string;
+import clsx from "clsx";
+import PhoneInput from "react-phone-input-2";
+
+interface PhoneNumberInputProps {
   name: string;
-  register?: any;
-  icon?: JSX.Element;
-  error?: any;
-  additionalClassname?: any;
-  value?: any;
-  disabled?: boolean;
+  placeholder?: string;
+  onChange: any;
+  label?: string;
+  value: string;
+  required?: boolean;
+  error?: string;
 }
 
 const InputFieldPhoneNumber = ({
-  type,
-  placeholder,
   name,
-  error,
-  register,
+  placeholder,
+  onChange,
+  value,
+  required,
   label,
-  icon,
-  additionalClassname,
-  disabled = false,
-}: IProps): JSX.Element => {
+  error,
+}: PhoneNumberInputProps) => {
   return (
-    <div className="">
-      <div className="w-full">
-        <span className="text-base text-[#333] font-medium">{label}</span>
-        <div
-          className={clsx({
-            ["flex justify-between items-center border border-solid border-gray-200 focus:border-gray-200 focus:outline-none placeholder:text-color-100 rounded-md overflow-hidden mt-1"]:
-              true,
-            [additionalClassname]: !!additionalClassname,
-          })}
-        >
-          {register ? (
-            <div className="flex items-center px-2 w-full">
-              <span className="w-fit pr-2 text-gray-900 inline-block border-r border-gray-200">
-                +234{" "}
-              </span>
-              <input
-                type={type}
-                id={name}
-                maxLength={9}
-                placeholder={placeholder}
-                disabled={disabled}
-                className="w-full h-10 pl-2 border-none pr-4 focus:outline-0 appearance-none leading-tight"
-                {...register(name)}
-              />
-            </div>
-          ) : (
-            <div className="flex items-center px-2 w-full">
-              <span className="w-fit pr-2 text-gray-400 inline-block border-r border-gray-200">
-                +234{" "}
-              </span>
-              <input
-                type={type}
-                id={name}
-                maxLength={9}
-                placeholder={placeholder}
-                disabled={disabled}
-                className="w-full h-10 pl-2 border-none pr-4 focus:outline-0 appearance-none leading-tight"
-              />
-            </div>
-          )}
-          {icon !== undefined ? (
-            <span className="px-2" aria-label="Open">
-              {icon}
-            </span>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
-      {error ? (
-        <p className="flex items-center lowercase text-[#ED2E7E] text-xs">
-          {error}
-        </p>
-      ) : null}
+    <div>
+      <span className="text-base text-[#333] font-medium">{label}</span>
+      <PhoneInput
+        value={value}
+        onChange={onChange}
+        countryCodeEditable
+        country={"ng"}
+        autoFormat
+        enableSearch
+        placeholder="Enter phone number"
+        inputClass={clsx({
+          ["flex justify-between items-center !w-full !border-none focus:outline-none placeholder:text-color-100 rounded-md overflow-hidden"]:
+            true,
+        })}
+        containerClass={clsx({
+          "border border-solid border-gray-200 focus:border-gray-200 focus:outline-none placeholder:text-color-100 rounded-md overflow-hidden mt-1 py-1":
+            true,
+          "border-red-600": !!error,
+        })}
+      />
     </div>
   );
 };
