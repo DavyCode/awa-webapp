7468a6e698697ece23ecc6e01648fecce5520ad5
okorieonyedikachi <cosmasdaniella@gmail.com>
2024-03-07 21:41:15 +0100

    Feat: ValuableOffers switch and form tabs

---
+++ src/components/ValubahPlans/ValubahOffers/ValubahOfferFormTabs.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useState } from "react";
+import Tabs from "../../Tabs";
+import SwitchFormEngine from "./ValubahOfferSwitchForm";
+
+const ValubahOfferFormTabs = () => {
+  const [currentTab, setCurrentTab] = useState("");
+
+  const [tabMenus, setTabs] = useState([
+    {
+      label: "Benefits",
+    },
+    {
+      label: "Details",
+    },
+    {
+      label: "Terms",
+    },
+  ]);
+  return (
+    <div className="mt-[23px] mb-[22px] mr-[20px]">
+      <Tabs
+        tabsData={tabMenus}
+        handleTabClick={(tabName) => setCurrentTab(tabName)}
+        activeTabLabel={currentTab}
+      />
+      <hr />
+      <SwitchFormEngine currentTab={currentTab} />
+    </div>
+  );
+};
+
+export default ValubahOfferFormTabs;
---
+++ src/components/ValubahPlans/ValubahOffers/ValubahOfferSwitchForm.tsx
@@ -0,0 +1,24 @@
+import BenefitsComponent from "./BenefitsCard.tsx";
+import DetailsCard from "./DetailsCard";
+import TermsCard from "./TermsCard";
+
+interface IProps {
+  currentTab: string;
+}
+const SwitchFormEngine: React.FC<IProps> = ({ currentTab }) => {
+  const currentDisplay = () => {
+    switch (currentTab) {
+      case "Benefits":
+        return <BenefitsComponent />;
+      case "Details":
+        return <DetailsCard />;
+      case "Terms":
+        return <TermsCard />;
+      default:
+        <p> data</p>;
+    }
+  };
+  return <div className="mt-[41px]">{currentDisplay()}</div>;
+};
+
+export default SwitchFormEngine;
