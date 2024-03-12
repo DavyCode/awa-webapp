d297f6fbb595fe146887da14033dd99578295995
Chima Ekeneme <sebastinechima@gmail.com>
2024-03-12 01:28:35 +0100

    make fixes during email verification

--- src/components/VerifyAccount/index.tsx
+++ src/components/VerifyAccount/index.tsx
@@ -5,19 +5,34 @@ import { useVerifyEmail } from "@/queries/auth-queries";
 import useToast from "@/hooks/useToast";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
+import { useCookies } from "react-cookie";
 
 const VerifyEmailComponent = () => {
   const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
   const queryParams = searchParams.toString();
-  const { errorToastHandler } = useToast();
-  const { isSuccess, mutate } = useVerifyEmail(errorToastHandler, () => {
-    // const params = searchParams.toString();
-    const params = new URLSearchParams(searchParams.toString());
-    params.set("is_verified", "yes");
-    router.replace(`${pathname}?${params.toString()}`);
-  });
+  const { errorToastHandler, successToastHandler } = useToast();
+
+  const [, setCookie] = useCookies(["awabah_tkn", "awabah_user_type"]);
+
+  const successCallback = (msg: string, data: { [key: string]: any }) => {
+    // const params = new URLSearchParams(searchParams.toString());
+    // params.set("is_verified", "yes");
+    // router.replace(`${pathname}?${params.toString()}`);
+    successToastHandler(msg);
+    setCookie("awabah_tkn", data.access_token);
+    setCookie("awabah_user_type", data?.user?.role, {
+      path: "/",
+    });
+    localStorage.setItem("awabah_udt", JSON.stringify(data?.user));
+
+    router.push("/dashboard");
+    // router.push(pathname)
+  };
+  const { isSuccess, mutate } = useVerifyEmail(
+    errorToastHandler,
+    successCallback,
+  );
 
   useEffect(() => {
     const params = new URLSearchParams(queryParams);
