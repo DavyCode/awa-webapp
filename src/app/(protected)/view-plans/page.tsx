ca548aafa34c0df197c2d727bcc7bbb069dbce5c
iAfolayan <iafolayanibikunle@gmail.com>
2024-03-12 19:20:09 +0100

    Implement sheet UI

--- src/app/(protected)/view-plans/page.tsx
+++ src/app/(protected)/view-plans/page.tsx
@@ -1,42 +1,55 @@
 'use client'
+import { useState } from 'react';
 import { useRouter } from 'next/navigation'
 import BackIcon from '@/assets/svg/back-icon.svg';
 import PlanCard from './card'
 import { PlanData } from '@/lib/data'
+import SheetComponent from '@/components/Modal/SheetComponent';
 
 const ViewPlans = () => {
     const router = useRouter()
+    const [isSheetOpen, setIsSheetOpen] = useState<boolean>(true)
+
+    const handleCloseSheet = () => setIsSheetOpen(false)
     return (
-      <div className="px-10 mt-12 mb-14">
-        <div className="w-full sm:w-[253px] space-y-4">
-          <div
-            className="w-full sm:w-[90px] flex gap-2 flex text-[#3D663D] cursor-pointer items-center"
-            onClick={() => router.back()}
-          >
-            <span className="w-4 h-4">
-              <BackIcon />
-            </span>
-            Go back
+      <>
+        <SheetComponent isOpen={isSheetOpen} handleClose={handleCloseSheet}>
+          <div className="overflow-y-auto h-full">
+            Content goes here
           </div>
-          <div className="text-sm text-[#C2C2C2]">
-            Dashboard /Total valuebah/
-            <span className="text-[#3D663D]">View plans</span>
+        </SheetComponent>
+
+        <div className="px-10 mt-12 mb-14">
+          <div className="w-full sm:w-[253px] space-y-4">
+            <div
+              className="w-full sm:w-[90px] flex gap-2 flex text-[#3D663D] cursor-pointer items-center"
+              onClick={() => router.back()}
+            >
+              <span className="w-4 h-4">
+                <BackIcon />
+              </span>
+              Go back
+            </div>
+            <div className="text-sm text-[#C2C2C2]">
+              Dashboard /Total valuebah/
+              <span className="text-[#3D663D]">View plans</span>
+            </div>
+            <h1 className="text-xl text-[#1A1A1A] font-medium">View Plans</h1>
           </div>
-          <h1 className="text-xl text-[#1A1A1A] font-medium">View Plans</h1>
+          <div className="flex flex-col sm:flex-row justify-between flex-wrap">
+            {PlanData.map(({ label, title, amount, desc }, index) => (
+              <PlanCard
+                label={label}
+                index={index}
+                title={title}
+                amount={amount}
+                desc={desc}
+                key={title}
+              />
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col sm:flex-row justify-between flex-wrap">
-          {PlanData.map(({ label, title, amount, desc }, index) => (
-            <PlanCard
-              label={label}
-              index={index}
-              title={title}
-              amount={amount}
-              desc={desc}
-              key={title}
-            />
-          ))}
-        </div>
-      </div>
+      </>
     );
 }
 
