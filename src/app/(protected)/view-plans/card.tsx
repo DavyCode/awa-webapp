d6d6bf94e5b92ad20cd4a183cf1337546995b624
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-03-14 00:13:55 +0100

    Chore: Multistep form 1

--- src/app/(protected)/view-plans/card.tsx
+++ src/app/(protected)/view-plans/card.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import { formatAmountVariant } from "@/lib/utils";
 import clsx from "clsx";
@@ -6,6 +6,7 @@ import ModalComponent from "@/components/Modal";
 import PurchaseFor from "@/components/ValubahPlan/PurchaseFor";
 import SheetComponent from "@/components/Modal/SheetComponent";
 import ValubahOffers from "@/components/ValubahPlan/ValubahOffers";
+import PlanDetails from "@/components/ValubahPlan/MultiStepForm/PlanDetails";
 
 interface PlanData {
   label: string;
@@ -48,20 +49,21 @@ const PlanCard: React.FC<PlanCardProps> = ({ planData }) => {
             "border-[#703061]": index === 3,
             "border-[#8050B8]": index === 4,
             "border-[#3C804B]": index === 5,
-            "border-[#C4C1C7]": index === 6
+            "border-[#C4C1C7]": index === 6,
           })}
         >
           <div className="w-full flex justify-end">
             <div
               className={clsx({
-                "text-xs text-white w-fit px-4 flex justify-center items-center rounded-[4px] sm:h-[26px]": true,
+                "text-xs text-white w-fit px-4 flex justify-center items-center rounded-[4px] sm:h-[26px]":
+                  true,
                 "bg-[#97912E]": index === 0,
                 "bg-[#A4813A]": index === 1,
                 "bg-[#354D72]": index === 2,
                 "bg-[#703061]": index === 3,
                 "bg-[#8050B8]": index === 4,
                 "bg-[#3C804B]": index === 5,
-                "bg-[#C4C1C7] text-black": index === 6
+                "bg-[#C4C1C7] text-black": index === 6,
               })}
             >
               {label}
@@ -95,6 +97,12 @@ const PlanCard: React.FC<PlanCardProps> = ({ planData }) => {
           setIsBeneficiaryModalOpen={setIsBeneficiaryModalOpen}
           planData={planData}
         />
+      </SheetComponent>
+      <SheetComponent
+        isOpen={isBeneficiaryModalOpen}
+        handleClose={closeBeneficiaryModal}
+      >
+        <PlanDetails planData={planData} />
       </SheetComponent>
     </>
   );
---
+++ src/components/ValubahPlan/MultiStepForm/PlanDetails.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { PlanCardProps } from "@/lib/data";
+import { formatAmountVariant } from "@/lib/utils";
+import InputField from "@/components/Forms/input-text";
+import { Button } from "@/components/ui/button";
+
+interface BeneficiaryProps {
+  planData: PlanCardProps;
+}
+
+const PlanDetails: React.FC<BeneficiaryProps> = (planData) => {
+  const [inputValue, setInputValue] = useState(4);
+  // const formattedAmount = Number(formatAmountVariant(planData.planData.amount));
+
+  return (
+    <div className=" mb-[67px] w-[476px]">
+      <div className=" ">
+        <h2>{planData.planData.title} </h2>
+        <span className="flex-1 mt-[9px]  flex justify-start text-[24px] font-bold leading-[32px] text-[#3D663D]">
+          ₦{formatAmountVariant(planData.planData.amount)} /month
+        </span>
+        <div className="bg-red-400 flex items-center w-full h-[20px] mt-[23px]">
+          jff
+        </div>
+      </div>
+      <div className="mt-[24px]">
+        <div className="mb-[16px]">
+          <InputField
+            type={"number"}
+            value={""}
+            name={"name"}
+            label="Enter number of beneficiaries"
+            placeholder="2"
+          />
+        </div>
+        <div>
+          <InputField
+            type={"type"}
+            name={"name"}
+            label="Do you have a promo code(Optional)"
+            placeholder="Enter promo code"
+          />
+        </div>
+
+        <div className="flex flex-col mt-[24px]">
+          <Button className="bg-[#DAFEDA] text-[#244D24] mb-[32px] py-[21px]">
+            {inputValue} X ₦{formatAmountVariant(planData.planData.amount)} =
+            ₦6,000/month
+          </Button>
+          <Button
+            onClick={() => {
+              console.log("true");
+            }}
+          >
+            Proceed
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlanDetails;
