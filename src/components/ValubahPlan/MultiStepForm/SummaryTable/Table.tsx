77ce5210944905124fd6ff6c5cb31059c78422f6
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-03-21 10:23:26 +0100

    missing data conditions

--- src/components/ValubahPlan/MultiStepForm/SummaryTable/Table.tsx
+++ src/components/ValubahPlan/MultiStepForm/SummaryTable/Table.tsx
@@ -36,7 +36,6 @@ const TableComponent = () => {
   return (
     <Table className="border-l border-y">
       <TableHeader
-        className=""
         style={{
           width: "182px",
           background:
@@ -45,7 +44,7 @@ const TableComponent = () => {
       >
         <TableRow>
           {headers.map((header, index) => (
-            <TableHead key={index} className="w-[200px]">
+            <TableHead key={index} className="font-[600]">
               {header.header}
             </TableHead>
           ))}
@@ -53,54 +52,215 @@ const TableComponent = () => {
       </TableHeader>
       <TableBody>
         {BeneficiarySummaryData.map((data, index) => (
-          <TableRow key={index}>
-            <TableCell className={data.name ? "font-medium" : "bg-red-600"}>
-              {data.name ? data.name : MissingData}
+          <TableRow key={index} className="">
+            <TableCell className="editCell ">
+              <h6>{data.name ? data.name : MissingData}</h6>
+              <p>Edit</p>
             </TableCell>
-            <TableCell>
+            <TableCell
+              style={
+                !data.phoneNumber
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.phoneNumber ? data.phoneNumber : MissingData}
             </TableCell>
-            <TableCell>{data.country ? data.country : MissingData}</TableCell>
-            <TableCell className={data.name ? "font-medium" : ""}>
+            <TableCell
+              style={
+                !data.country
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
+              {data.country ? data.country : MissingData}
+            </TableCell>
+            <TableCell
+              className="text-center"
+              style={
+                !data.stateOfOrigin
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.stateOfOrigin ? data.stateOfOrigin : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.lga
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.lga ? data.lga : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.qualification
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.qualification ? data.qualification : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.age
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.age ? data.age : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.maritalStatus
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.maritalStatus ? data.maritalStatus : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.rsaNumber
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.rsaNumber ? data.rsaNumber : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.image
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.image ? data.image : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.nokName
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.nokName ? data.nokName : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.nokEmail
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.nokEmail ? data.nokEmail : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.nokPhoneNumber
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.nokPhoneNumber ? data.nokPhoneNumber : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.nokGender
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.nokGender ? data.nokGender : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.relationship
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.relationship ? data.relationship : MissingData}
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell
+              className="text-center"
+              style={
+                !data.occupation
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.occupation ? data.occupation : MissingData}
             </TableCell>
-            <TableCell className="" style={{ width: "max-content" }}>
+            <TableCell
+              className=" w-[5rem]"
+              style={
+                !data.nokAddress
+                  ? {
+                      backgroundColor: "#FFF2F1",
+                      color: "#EA4335",
+                    }
+                  : undefined
+              }
+            >
               {data.nokAddress ? data.nokAddress : MissingData}
             </TableCell>
           </TableRow>
